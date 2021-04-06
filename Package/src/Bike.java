790f3aa43f1c7d71e9ae53c86b2d2f4eb6057e17
Navaneetu <Navaneetu@LAPTOP-IIPJCH1C>
2021-04-06 21:02:12 +0530

    Polymorphism

---
+++ Package/src/Bike.java
@@ -0,0 +1,10 @@
+
+public class Bike {
+	class Honda1 extends Bike{  
+		  void run(){System.out.println("running safely with 100kmph");}  
+		    
+		  public  void main(String args[]){  
+		  Honda1 honda= new Honda1();  
+		  honda.run();  
+		  }  
+
