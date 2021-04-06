385b1afa7d132ad8191d00d7a3a2eac7a3cb07d3
Navaneetu <Navaneetu@LAPTOP-IIPJCH1C>
2021-04-06 19:56:49 +0530

    Class , Object , constructor

---
+++ oops -object and classes/src/student.java
@@ -0,0 +1,15 @@
+
+public class student {
+	int id;
+	String name;  
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		student s1=new student();
+		System.out.println(s1.id);
+		System.out.println(s1.name);  
+
+	}
+
+}
+
---
+++ oops -object and classes/src/student1.java
@@ -0,0 +1,14 @@
+
+public class student1 {
+	int id;  
+	String name;  
+
+}
+class Teststudent{  
+	public static void main(String args[]){  
+		Teststudent s1=new student();  
+		System.out.println(s1.id);  
+		System.out.println(s1.name);  
+	}  
+
+}
